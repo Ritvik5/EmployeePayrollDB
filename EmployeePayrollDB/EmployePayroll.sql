fb9e68b7b40f6c5cdd5ed541ad8c7d50bca99ceb
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 21:22:17 +0530

    Inserting Records using insert Query

--- EmployeePayrollDB/EmployePayroll.sql
+++ EmployeePayrollDB/EmployePayroll.sql
@@ -14,3 +14,9 @@ StartDate date not null,
 )
 
 
+insert into EmployeePayroll values 
+('Jack',25000,'2019-05-08'),
+('Smith',15000,'2022-01-01'),
+('John',35000,'2019-08-19'),
+('Snow',45000,'2018-09-20');
+
