805d616997573a79db6adb30cbfbbb54ed48809a
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 21:14:19 +0530

    Creating Table and set ID as Primary Key

--- EmployeePayrollDB/EmployePayroll.sql
+++ EmployeePayrollDB/EmployePayroll.sql
@@ -3,3 +3,14 @@
 select Name from sys.databases;
 
 use Payroll;
+
+
+create table EmployeePayroll
+(
+ID int identity(1,1) primary key,
+Name Varchar(100) not null,
+Salary money not null,
+StartDate date not null,
+)
+
+
